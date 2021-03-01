85de204a40f914832cc42f9eeab5f047948cc673
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2021-03-01 14:14:57 -0700

    Updated flow to grab all data from jcr:content node

--- find-cfs.sh
+++ find-cfs.sh
@@ -12,19 +12,25 @@ PORT=4502
 # Do not change anything below this line
 
 PATH_PARAM="path=/content/dam"
-TYPE_PARAM="type=dam:Asset"
+#TYPE_PARAM="type=dam:Asset"
 
-PRED1_KEY="1_property=@jcr:content/contentFragment"
+PRED1_KEY="1_property=contentFragment"
 PRED1_VAL="1_property.value=true"
 PRED1_PARAMS="${PRED1_KEY}&${PRED1_VAL}"
 
-# Use pager for better performance
-# TODO: implement pagination calls
-LIMIT="p.limit=100&guessTotal=true"
+PRED2_KEY="2_property=jcr:primaryType"
+PRED2_VAL="2_property.value=dam:AssetContent"
+PRED2_PARAMS="${PRED2_KEY}&${PRED2_VAL}"
 
-QUERY="${PATH_PARAM}&${TYPE_PARAM}&${PRED1_PARAMS}&${LIMIT}"
+LIMIT="p.limit=100&p.guessTotal=true"
+
+# control how much data to return
+NODE_DATA="p.hits=full&p.nodedepth=1"
+
+QUERY="${PATH_PARAM}&${PRED2_PARAMS}&${PRED1_PARAMS}&${NODE_DATA}&${LIMIT}"
 REQUEST="http://${HOST}:${PORT}/bin/querybuilder.json?${QUERY}"
 
+# TODO: implement pagination calls
 curl -s \
     -u ${USER}:${PASS} \
     ${REQUEST} | jq .
