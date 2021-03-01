15384d321d0ba478f05b37c372fdca110031d7ae
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2021-03-01 10:14:39 -0700

    initial working query

--- find-cfs.sh
+++ find-cfs.sh
@@ -5,6 +5,20 @@ PASS=admin
 HOST=localhost
 PORT=4502
 
+PRED1_KEY="1_property=jcr:primaryType"
+PRED1_VAL="1_property.value=dam:Asset"
+PRED1_OP="1_property.operation=like"
+PRED1_PARAMS="${PRED1_KEY}&${PRED1_VAL}&${PRED1_OP}"
+
+PRED2_KEY="2_property=contentFragment"
+PRED2_VAL="2_property.value=true"
+PRED2_PARAMS="${PRED2_KEY}&${PRED2_VAL}"
+
+#QUERY="path=/content/dam&${PRED1_PARAMS}&${PRED2_PARAMS}"
+QUERY="path=/content/dam&${PRED2_PARAMS}"
+
 curl -s \
     -u ${USER}:${PASS} \
-    "http://${HOST}:${PORT}/bin/querybuilder.json" | jq .
+    "http://${HOST}:${PORT}/bin/querybuilder.json?${QUERY}" | jq .
+
+echo "Query: ${QUERY}"
